d6bec50890bead922627729c0e51ad1e1751bb07
Leonid Smirnov <remakeleonardo@gmail.com>
2017-05-19 02:27:35 +0300

    first version

---
+++ main.cpp
@@ -0,0 +1,339 @@
+#include <iostream>
+
+using namespace std;
+
+class Point {
+private:
+    
+    double x, y;
+    static double dx, dy;
+    
+public:
+    // дефолтный конструктор, для чтения с консоли
+    Point(): x(0), y(0) {
+        dx = 1e-5;
+        dy = 1e-5;
+    }
+    
+    Point(const double & _x, const double & _y): x(_x), y(_y) {
+        dx = 1e-5;
+        dy = 1e-5;
+    }
+    
+    Point(const Point & pnt);
+    
+    Point & operator =(const Point & pnt);
+    
+    Point operator +(const Point & pnt) const;
+    Point operator -(const Point & pnt) const;
+    
+    Point & operator +=(const Point & pnt);
+    Point & operator -=(const Point & pnt);
+    
+    Point & operator ++(); // префиксный
+    const Point operator ++(int); // постфиксный
+    
+    Point & operator --(); // префиксный
+    const Point operator --(int); // постфиксный
+    
+    void setX(const double & _x);
+    void setY(const double & _y);
+    
+    double getX() const;
+    double getY() const;
+    
+    friend istream & operator >>(istream & in, Point & pnt);
+    friend ostream & operator <<(ostream & in, const Point & pnt);
+    
+};
+
+Point::Point(const Point & pnt) {
+    this->x = pnt.x;
+    this->y = pnt.y;
+}
+
+Point & Point::operator =(const Point & pnt) {
+    if (this == &pnt)
+        return *this;
+    
+    this->x = pnt.x;
+    this->y = pnt.y;
+    
+    return *this;
+}
+
+Point Point::operator +(const Point & pnt) const {
+    return Point((this->x + pnt.x), (this->y + pnt.y));
+}
+
+Point Point::operator -(const Point & pnt) const {
+    return Point((this->x - pnt.x), (this->y - pnt.y));
+}
+
+Point & Point::operator +=(const Point & pnt) {
+    this->x += pnt.x;
+    this->y += pnt.y;
+    return *this;
+}
+
+Point & Point::operator -=(const Point & pnt) {
+    this->x -= pnt.x;
+    this->y -= pnt.y;
+    return *this;
+}
+
+Point & Point::operator ++() {
+    this->x += dx;
+    this->y += dy;
+    return *this;
+}
+
+const Point Point::operator ++(int n) {
+    Point pnt = *this;
+    ++(*this);
+    return pnt;
+}
+
+Point & Point::operator --() {
+    this->x -= dx;
+    this->y -= dy;
+    return *this;
+}
+
+const Point Point::operator --(int n) {
+    Point pnt = *this;
+    --(*this);
+    return pnt;
+}
+
+void Point::setX(const double & _x) {
+    this->x = _x;
+}
+
+void Point::setY(const double & _y) {
+    this->y = _y;
+}
+
+double Point::getX() const {
+    return this->x;
+}
+
+double Point::getY() const {
+    return this->y;
+}
+
+istream & operator >>(istream & in, Point & pnt) {
+    in >> pnt.x;
+    in >> pnt.y;
+    return in;
+}
+
+ostream & operator <<(ostream & out, const Point & pnt) {
+    out << "(" << pnt.x << ";" << pnt.y << ")";
+    return out;
+}
+
+class Figura {
+protected:
+    
+    Point cg, cz;
+    
+public:
+    
+    Figura(const Point & p): cg(p), cz(p) {};
+    
+    Figura(const Figura & _figura) {
+        this->cg = _figura.cg;
+        this->cz = _figura.cz;
+    };
+    
+    Figura & operator =(const Figura & _figura);
+    
+    virtual void show() {};
+    
+    void hide() {};
+    
+    void move(const Point &) {};
+    
+    void move() {};
+    
+    void trek() {};
+    
+    virtual ~Figura() {};
+};
+
+Figura & Figura::operator =(const Figura & _figura) {
+    if (this == &_figura)
+        return *this;
+    
+    this->cg = _figura.cg;
+    this->cz = _figura.cz;
+    
+    return *this;
+}
+
+
+class Krug: public Figura {
+private:
+    
+    double r;
+    
+public:
+    
+    Krug(const Point & _p, const double & _r): Figura(_p), r(_r) {};
+    
+    Krug(const Krug & kr);
+    
+    Krug & operator =(const Krug & kr);
+    
+    virtual void show() {};
+    
+    void setR(const double & _r);
+    
+    double getR() const;
+    
+    ~Krug() {};
+};
+
+Krug::Krug(const Krug & kr): Figura(kr.cg) {
+    this->r = kr.r;
+}
+
+Krug & Krug::operator =(const Krug & kr) {
+    if (this == &kr)
+        return *this;
+    
+    this->cz = kr.cz;
+    this->cg = kr.cg;
+    this->r = kr.r;
+    return *this;
+}
+
+void Krug::setR(const double & _r) {
+    r = _r;
+}
+
+double Krug::getR() const {
+    return r;
+}
+
+
+class Ellipse: public Figura {
+private:
+    
+    double rx, ry;
+    
+public:
+    
+    Ellipse(const Point & _p, const double & _rx, const double & _ry): Figura(_p), rx(_rx), ry(_ry) {};
+    
+    Ellipse(const Ellipse & ellipse);
+    
+    Ellipse & operator =(const Ellipse & ellipse);
+    
+    virtual void show() {};
+    
+    void setRx(const double & _rx);
+    void setRy(const double & _ry);
+    
+    double getRx() const;
+    double getRy() const;
+    
+    ~Ellipse() {};
+    
+};
+
+Ellipse::Ellipse(const Ellipse & ellipse): Figura(ellipse.cz), rx(ellipse.rx), ry(ellipse.ry) {}
+
+Ellipse & Ellipse::operator =(const Ellipse & ellipse) {
+    if (this == &ellipse)
+        return *this;
+    
+    this->cg = ellipse.cg;
+    this->cz = ellipse.cz;
+    this->rx = ellipse.rx;
+    this->ry = ellipse.ry;
+    
+    return *this;
+}
+
+void Ellipse::setRx(const double & _rx) {
+    this->rx = _rx;
+}
+
+void Ellipse::setRy(const double & _ry) {
+    this->ry = _ry;
+}
+
+double Ellipse::getRx() const {
+    return this->rx;
+}
+
+double Ellipse::getRy() const {
+    return this->ry;
+}
+
+
+class Box: public Figura {
+private:
+    
+    double a, b;
+    
+public:
+    
+    Box(const Point & _p, const double & _a, const double & _b): Figura(_p), a(_a), b(_b) {};
+    
+    Box(const Box & box);
+    
+    Box & operator =(const Box & box);
+    
+    virtual void show() {};
+    
+    void setA(const double & _a);
+    void setB(const double & _b);
+    
+    double getA() const;
+    double getB() const;
+    
+    ~Box() {};
+    
+};
+
+Box::Box(const Box & box): Figura(box.cg), a(box.a), b(box.b) {}
+
+Box & Box::operator =(const Box & box) {
+    if (this == &box)
+        return *this;
+    
+    this->cg = box.cg;
+    this->cz = box.cz;
+    this->a = box.a;
+    this->b = box.b;
+    
+    return *this;
+}
+
+void Box::setA(const double & _a) {
+    this->a = _a;
+}
+
+void Box::setB(const double & _b) {
+    this->b = _b;
+}
+
+double Box::getA() const {
+    return this->a;
+}
+
+double Box::getB() const {
+    return this->b;
+}
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    Point p;
+    Figura f(p);
+    f.hide();
+    return 0;
+}
